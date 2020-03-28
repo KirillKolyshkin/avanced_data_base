539f1b3a675bed844c6f8a27281a0d5d0687780e
KirillKolyshkin <40914279+KirillKolyshkin@users.noreply.github.com>
2020-03-29 00:11:43 +0400

    Update task 3.sql

--- task 3.sql
+++ task 3.sql
@@ -1,3 +1,4 @@
+
 create table users
 (
     id   integer,
@@ -8,7 +9,7 @@ create index idx_users on users (id);
 CREATE OR REPLACE FUNCTION createTable(tableName varchar) RETURNS VOID AS
 $$
 BEGIN
-    EXECUTE format('CREATE TABLE IF NOT EXISTS %s () INHERITS(users)', tableName);
+    EXECUTE format('CREATE TABLE IF NOT EXISTS %s () INHERITS(users);', tableName);
 END;
 $$ language plpgsql;
 
@@ -16,18 +17,28 @@ CREATE OR REPLACE FUNCTION addRule(tableName varchar, startId integer, finishId 
 $$
 BEGIN
     EXECUTE format(
-            'CREATE OR REPLACE RULE redirect_insert_to_%s AS ON INSERT TO users WHERE NEW.id BETWEEN %s AND %s DO INSTEAD INSERT INTO %s VALUES (NEW.id, NEW.name)',
+            'CREATE OR REPLACE RULE redirect_insert_to_%s AS ON INSERT TO users WHERE NEW.id BETWEEN %s AND %s DO INSTEAD INSERT INTO %s VALUES (NEW.id, NEW.name);',
             tableName, startId, finishId, tableName);
 END ;
 $$ language plpgsql;
 
-CREATE OR REPLACE FUNCTION addRuleForUpdate(tableName varchar, startId integer, finishId integer,
-                                            addingcost integer) RETURNS VOID AS
+CREATE OR REPLACE FUNCTION addPartionCheck(tableName varchar, startId integer, finishId integer) RETURNS VOID AS
 $$
 BEGIN
     EXECUTE format(
-            'CREATE OR REPLACE RULE redirect_update_to_%s AS ON UPDATE TO users WHERE NEW.id BETWEEN %s AND %s DO INSTEAD (INSERT INTO users VALUES ((random() * (999999 + 1)), NEW.name); DELETE from users where id = NEW.id)',
-            tableName, startId, finishId, addingcost);
+            'ALTER TABLE %s ADD CONSTRAINT partition_check_%s CHECK (id >= %s and id <= %s);',
+            tableName, tableName, startId, finishId);
+END ;
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION addRuleForUpdate(tableName varchar, startId integer, finishId integer,
+                                            addingCost integer) RETURNS VOID AS
+$$
+BEGIN
+    EXECUTE format(
+            'CREATE OR REPLACE RULE redirect_update_to_%s AS ON UPDATE TO users WHERE NEW.id BETWEEN %s AND %s DO INSTEAD(
+            INSERT INTO users VALUES (NEW.id + %s, NEW.name); DELETE FROM %s WHERE id = NEW.id)',
+            tableName, startId, finishId, addingCost, tableName);
 END ;
 $$ language plpgsql;
 
@@ -61,6 +72,38 @@ FROM
 SELECT *
 FROM
     createTable('users_10');
+
+SELECT *
+FROM
+    addPartionCheck('users_1', 0, 99999);
+SELECT *
+FROM
+    addPartionCheck('users_2', 100000, 199999);
+SELECT *
+FROM
+    addPartionCheck('users_3', 200000, 299999);
+SELECT *
+FROM
+    addPartionCheck('users_4', 300000, 399999);
+SELECT *
+FROM
+    addPartionCheck('users_5', 400000, 499999);
+SELECT *
+FROM
+    addPartionCheck('users_6', 500000, 599999);
+SELECT *
+FROM
+    addPartionCheck('users_7', 600000, 699999);
+SELECT *
+FROM
+    addPartionCheck('users_8', 700000, 799999);
+SELECT *
+FROM
+    addPartionCheck('users_9', 800000, 899999);
+SELECT *
+FROM
+    addPartionCheck('users_10', 900000, 999999);
+
 
 SELECT *
 FROM
@@ -102,13 +145,13 @@ FROM
     addRuleForUpdate('users_2', 100000, 199999, 100000);
 SELECT *
 FROM
-    addRuleForUpdate('users_3', 200000, 299999,100000);
+    addRuleForUpdate('users_3', 200000, 299999, 100000);
 SELECT *
 FROM
-    addRuleForUpdate('users_4', 300000, 399999,100000);
+    addRuleForUpdate('users_4', 300000, 399999, 100000);
 SELECT *
 FROM
-    addRuleForUpdate('users_5', 400000, 499999,100000);
+    addRuleForUpdate('users_5', 400000, 499999, 100000);
 SELECT *
 FROM
     addRuleForUpdate('users_6', 500000, 599999, 100000);
@@ -125,16 +168,17 @@ SELECT *
 FROM
     addRuleForUpdate('users_10', 900000, 999999, -900000);
 
-CREATE FUNCTION insert() RETURNS void AS $$
+Drop function insert();
+CREATE FUNCTION insert() RETURNS void AS
+$$
 DECLARE
-  l int = 0;
-  h int = 999999;
+    t int = 0;
 BEGIN
-  FOR i IN l..h
-  LOOP
-    INSERT INTO users (id, name) VALUES (random() * (999999 + 1), 'initname');
-  END LOOP;
+    FOR i IN 0..999999
+        LOOP
+            t = random() * 999999;
+            INSERT INTO users (id, name) VALUES (t, 'initname');
+        END LOOP;
 END;
 $$ LANGUAGE plpgsql;
-
 SELECT insert();
