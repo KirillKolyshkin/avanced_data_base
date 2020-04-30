7ba38ff25eeb90b77ba73e21bb5a9b7293d90465
KirillKolyshkin <40914279+KirillKolyshkin@users.noreply.github.com>
2020-04-30 16:44:08 +0400

    Update new_indexes.sql

--- new_indexes.sql
+++ new_indexes.sql
@@ -37,8 +37,6 @@ INSERT INTO table_with_gist (id, content)
 SELECT  i, random_string_new(100) FROM generate_series(1, 1000000) as t(i);
 
 CREATE EXTENSION pg_trgm;
-
-drop index idx_gin_table;
 CREATE INDEX CONCURRENTLY idx_gin_table ON table_with_gin USING gin(content gin_trgm_ops);
 CREATE INDEX CONCURRENTLY idx_gist_table ON table_with_gist USING gist(content gist_trgm_ops);
 CREATE INDEX CONCURRENTLY idx_btree_table ON table_with_btree (content);
